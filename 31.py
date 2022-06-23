329b26915558194e98558d08ba72fc0312a399df
William Kerby <w.kerby99@gmail.com>
2022-06-23 14:13:57 -0400

    change cardsuit from Spades to Bananas

--- 31.py
+++ 31.py
@@ -1,7 +1,7 @@
 #simulate the 31 card game in python
 #begin by creating a dictionary for a deck of cards
 card_deck = {}
-suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+suits = ["Hearts", "Clubs", "Diamonds", "Bananas"]
 cards = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
 for suit in suits:
 	card_deck[suit] = cards
