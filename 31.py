7d6d7302422ad2c995a73aa627e74673f170d3d3
William Kerby <w.kerby99@gmail.com>
2022-06-23 14:23:46 -0400

    add procedural code for player list creation

--- 31.py
+++ 31.py
@@ -1,8 +1,11 @@
 #simulate the 31 card game in python
 #begin by creating a dictionary for a deck of cards
 card_deck = {}
-suits = ["Hearts", "Clubs", "Diamonds", "Bananas"]
+bananas = ["red", "yellow", "blue", "green"]
 cards = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
-for suit in suits:
-	card_deck[suit] = cards
+for banana in bananas:
+	card_deck[banana] = cards
+
+#create a procedural version of a player list
+playerlist = ["player1","player2"]
 
