c1ac915c680c8a4cfb0ef4315391e0a2e3c893c2
Will Kerby <w.kerby99@gmail.com>
2022-06-23 13:41:54 -0400

    add player class to 31.py script

--- 31.py
+++ 31.py
@@ -1,7 +1,17 @@
 #simulate the 31 card game in python
+import random
 #begin by creating a dictionary for a deck of cards
 card_deck = {}
 suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
 cards = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
 for suit in suits:
 	card_deck[suit] = cards
+#create player profile class
+class Player:
+	personality = ["Conservative", "Moderate", "Risk-Taker"]
+	def __init__(self,name):
+		self.name = name
+		self.personality = random.choice(Player.personality)
+
+Player1 = Player("Player1")
+print(Player1.personality)
